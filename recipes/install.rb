89e9c4c9f07247da775a87eb242796cb3c46de33
Ranjib Dey <ranjib@pagerduty.com>
2015-02-15 01:05:04 -0800

    refactor for upstream lxc release

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,20 +1,16 @@
-include_recipe 'build-essential'
-
-execute 'apt-get update -y' do
-  not_if 'dpkg -s lxc'
-end
-
-%w{lxc liblxc0 lxc-templates lxc-dev python3-lxc}.each do |pkg|
+%w{
+  lxc
+  lxc-templates
+  lxc-dev
+  python3-lxc
+  cgmanager-utils
+  build-essential
+  }.each do |pkg|
   package pkg
 end
 
-lxc_gem_path = ::File.join(Chef::Config[:file_cache_path],'ruby-lxc-0.1.0.gem')
-
-cookbook_file lxc_gem_path do
-  source 'ruby-lxc-0.1.0.gem'
-end.run_action(:create)
-
-gem_package 'ruby-lxc' do
-  source lxc_gem_path
-  gem_binary '/opt/chef/embedded/bin/gem'
+%w{ruby-lxc lxc-extra chef-lxc}.each do |gem|
+  gem_package gem do
+    gem_binary '/opt/chef/embedded/bin/gem'
+  end
 end
